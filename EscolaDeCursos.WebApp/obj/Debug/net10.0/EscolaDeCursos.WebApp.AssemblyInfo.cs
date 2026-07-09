768c00fea12c9b7dc0607cf027659e67c7d06250
Kauan Silva <kauanmacedo338@gmail.com>
2026-07-09 17:07:33 -0300

    Adiciona: Editar, Excluir

--- EscolaDeCursos.WebApp/obj/Debug/net10.0/EscolaDeCursos.WebApp.AssemblyInfo.cs
+++ EscolaDeCursos.WebApp/obj/Debug/net10.0/EscolaDeCursos.WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EscolaDeCursos.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d456bf1998c9202a8a034c83dcaf69293eef9ebe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b03c239ee2f4fff8e97ee0220129aa46c550bdfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("EscolaDeCursos.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EscolaDeCursos.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
