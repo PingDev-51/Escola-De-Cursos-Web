99971cafaed0dda192e32606b9fd251df3a834d1
Kauan Silva <kauanmacedo338@gmail.com>
2026-07-09 22:17:08 -0300

    Feito Navbar

--- EscolaDeCursos.WebApp/obj/Debug/net10.0/EscolaDeCursos.WebApp.AssemblyInfo.cs
+++ EscolaDeCursos.WebApp/obj/Debug/net10.0/EscolaDeCursos.WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EscolaDeCursos.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2df5369250a310211ed523155c0aa1bb33765fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d71fa5d37d2cc121c08d7a470831a95ef375571")]
 [assembly: System.Reflection.AssemblyProductAttribute("EscolaDeCursos.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EscolaDeCursos.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
