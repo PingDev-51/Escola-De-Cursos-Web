c68d5db5f989f426658250364b34ea40ced343a1
Kauan Galvani <kauanrafaelgalvani@gmail.com>
2026-07-10 16:00:10 -0300

    organizado detalhe final de instrutores

--- EscolaDeCursos.WebApp/obj/Debug/net10.0/EscolaDeCursos.WebApp.AssemblyInfo.cs
+++ EscolaDeCursos.WebApp/obj/Debug/net10.0/EscolaDeCursos.WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EscolaDeCursos.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+876c16c3e79d385de00b27d0248e368a8f5c5a8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+932cb280f0ddd8a1769b9b27ecb98d37ded2641f")]
 [assembly: System.Reflection.AssemblyProductAttribute("EscolaDeCursos.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EscolaDeCursos.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
