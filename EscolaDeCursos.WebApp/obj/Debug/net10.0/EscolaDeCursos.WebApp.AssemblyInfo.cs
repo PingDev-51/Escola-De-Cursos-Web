67c4e42c435484bf5a8dac1ed9644bab1da21773
Kauan Silva <kauanmacedo338@gmail.com>
2026-07-09 21:38:20 -0300

    adicionado repositorio orm base

--- EscolaDeCursos.WebApp/obj/Debug/net10.0/EscolaDeCursos.WebApp.AssemblyInfo.cs
+++ EscolaDeCursos.WebApp/obj/Debug/net10.0/EscolaDeCursos.WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EscolaDeCursos.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8737b40f4f8586d52157e91283e4a4e2ec15b533")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdc7dced8a498496a1850117797d6ef1bf45d38b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EscolaDeCursos.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EscolaDeCursos.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
