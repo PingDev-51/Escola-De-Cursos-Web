07d68d6bf6cab793c2028aba4763bf36c521fe18
Kauan Galvani <kauanrafaelgalvani@gmail.com>
2026-07-11 15:15:32 -0300

    adicionado mapeamento ao listar e cadastrar categorias

--- EscolaDeCursos.WebApp/Modulos/ModuloCategoria/CategoriaProfile.cs
+++ EscolaDeCursos.WebApp/Modulos/ModuloCategoria/CategoriaProfile.cs
@@ -1,8 +1,14 @@
 using System;
+using AutoMapper;
+using EscolaDeCursos.Aplicacao.Modulos.ModuloCategoria;
 
 namespace EscolaDeCursos.WebApp.Modulos.ModuloCategoria;
 
-public class CategoriaProfile
+public class CategoriaProfile : Profile
 {
-
+    public CategoriaProfile()
+    {
+        CreateMap<ListarCategoriaDto, ListarCategoriaViewModel>();
+        CreateMap<CadastrarCategoriaViewModel, CadastrarCategoriaDto>();
+    }
 }
